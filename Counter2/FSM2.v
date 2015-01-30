c927159ef4d3aaefcf0bfc9085f67d63e9f1831c
John Beale <beale@bealecorner.com>
2015-01-29 20:29:04 -0800

    debug work on FSM for shift reg. control

--- Counter2/FSM2.v
+++ Counter2/FSM2.v
@@ -1,7 +1,7 @@
 // Synchronous Finite-State Machine for simple output-only SPI Master
 // based on http://inst.eecs.berkeley.edu/~cs150/fa08/Documents/FSM.pdf
 
-module SimpleFSM (
+module SerialCTL (
   input wire  Clock,
   input wire  Reset,
   input wire  Start,
@@ -9,10 +9,13 @@ module SimpleFSM (
   
   output wire SS,      // SPI slave select (active low)
   output wire SCLK,    // SPI clock output
-  output wire MOSI     // SPI MOSI serial data output
+  output wire MOSI,     // SPI MOSI serial data output
+  output wire DoneFlag,  // 1-clk pulse indicating done
+  output wire [2:0] CurrentStateOut, // DEBUG
+  output wire Gclock // gated clock from pulsegate
   );
   
-parameter BITS = 32;  // how many bits in one SPI output word
+parameter BITS = 4;  // how many bits in one SPI output word
 localparam MSB = BITS-1;  // count bits from 0 to MSB
 
 // Names of each possible state of the FSM
@@ -26,46 +29,81 @@ localparam STATE_Initial = 3'd0,
 		   STATE_7 = 3'd7;
 		   
 // Registers holding value of current and future state		   
-reg[2:0]  Currentstate;
+reg[2:0]  CurrentState;
 reg[2:0]  NextState;
 
-reg  SSreg;  // store value of SS/ output
-reg  [MSB:0] SRdata; // the data being shifted out
+wire PGrun;     // run signal to pulsegate
+// wire Gclock;  // gated clock signal
+wire DoneSR; // done flag from pulsegate
+// wire [2:0] CurrentStateOut; // DEBUG
+
+// ==============================================
+// generate pulse train with fixed number COUNT of clocks
+pulsegate #(.COUNT(4)) pg1 (
+    .clk(Clock),     // clock input to be gated 
+    .run(PGrun),     // low=reset; high starts output pulse train
+    .gclk(Gclock),   // gated output pulses
+    .done(DoneSR)    // high when pulse train complete
+    );
+
+// =============================================================
+
+//reg  SSreg;  // store value of SS/ output
+//reg  [MSB:0] SRdata; // the data being shifted out
  
 // signals from combinatorial logic
-assign SCLK = Clock & ^SS;  // output clock is gated by SS/ (select line active low) 
-assign SS = SSreg;          // SS/ driven from register
-  
-always@(posedge Clock) begin
-  if (Reset) CurrentState <= STATE_Initial;
-  else CurrentState <= NextState;
-end
+//assign SCLK = Clock & ^SS;  // output clock is gated by SS/ (select line active low) 
 
-always@( CurrentState or Start ) begin
+assign SS = (CurrentState == STATE_Initial);          // SS/ driven from register
+assign PGrun = (CurrentState == STATE_1);  // pulsgate RUN signal
+assign DoneFlag = ( CurrentState == STATE_4);  // last state before IDLE
+assign CurrentStateOut = CurrentState; // DEBUG
+
+// =============================================================
+// State Engine logic for changing states
+// inputs: CurrentState, Start, DoneSR, Reset
+// outputs: NextState
+
+always@( * ) begin  
+ if(Reset == 1'b1)
+    NextState = STATE_Initial;
+ else begin
   NextState = CurrentState;
   case (CurrentState)
     STATE_Initial: begin  // idle state
-	  SSreg = 1'b1;      // SS/ signal is inactive in idle state
+//	  SSreg = 1'b1;      // SS/ signal is inactive in idle state
 	  if (Start)  NextState = STATE_1;  // Start is the only way out of idle
 	end
-	STATE_1: begin    // we have received 'Start' and begin the data output
-	  SRdata = Data;  // parallel-load the shift register from input bus
-	  SSreg = 1'b0;   // bring SS/ signal active
+	STATE_1: begin    // we have received 'Start' : parallel load shift reg.
+                      // and signal output is starting
 	  NextState = STATE_2;
 	end
-	STATE_2: begin
-	  // do various stuff
-	  NextState = STATE_3;
+	STATE_2: begin  // shift register is actively shifting data out
+	  if (DoneSR)  NextState = STATE_3;
 	end
-	STATE_3: begin
-	  // do various stuff
-	  NextState = STATE_4;
+	STATE_3: begin // SR is now done
+      NextState = STATE_4;
 	end	
-	STATE_4: begin
-	  // do various stuff
+	STATE_4: begin // final state, return to IDLE after this
+	  NextState = STATE_Initial;
+	end	
+	STATE_5: begin // unused
+	  NextState = STATE_Initial;
+	end	
+	STATE_6: begin // unused
+	  NextState = STATE_Initial;
+	end	
+	STATE_7: begin // unused
 	  NextState = STATE_Initial;
 	end	
   endcase
- end
+ end // if-else
+end // always
  // --------------------------------------------
+ always@(posedge Clock) begin  // one tick forward of the state engine
+  if (Reset) CurrentState <= STATE_Initial;
+  else CurrentState <= NextState;
+end
+// =============================================================
+
  endmodule
---
+++ Counter2/SR_FSM_TB.v
@@ -0,0 +1,94 @@
+`timescale 1ns / 10ps
+
+////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer:
+//
+// Create Date:   17:45:50 01/29/2015
+// Design Name:   SerialCTL
+// Module Name:   C:/Users/John/ShiftRegControl-FSM-_TB/SR_FSM_TB.v
+// Project Name:  ShiftRegControl-FSM-_TB
+// Target Device:  
+// Tool versions:  
+// Description: 
+//
+// Verilog Test Fixture created by ISE for module: SerialCTL
+//
+// Dependencies:
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+////////////////////////////////////////////////////////////////////////////////
+
+module SR_FSM_TB;
+
+	// Inputs
+	reg Clock;
+	reg Reset;
+	reg Start;
+	reg [31:0] Data;
+
+	// Outputs
+	wire SS;
+	wire SCLK;
+	wire MOSI;
+	wire DoneFlag;
+	wire [2:0] State;
+   wire Gclock;
+	
+	// Instantiate the Unit Under Test (UUT)
+	SerialCTL uut (
+		.Clock(Clock), 
+		.Reset(Reset), 
+		.Start(Start), 
+		.Data(Data), 
+		.SS(SS), 
+		.SCLK(SCLK), 
+		.MOSI(MOSI), 
+		.DoneFlag(DoneFlag),
+		.CurrentStateOut(State), // DEBUG
+		.Gclock(Gclock)
+	);
+
+	always
+		  #2 Clock = !Clock;
+
+	initial 
+	begin
+		// Initialize Inputs
+		Clock = 0;
+		Reset = 0;
+		Start = 0;
+		Data = 0;
+
+	   $display("starting simulation for Pulsegate circuit...");
+
+		// Wait 100 ns for global reset to finish
+		#100
+        
+		#102 Reset = 1'b1;
+		#107 Reset = 1'b0;
+		
+		// Add stimulus here		  
+    
+	   #119 Start = 1'b1;  // bring RUN to true
+		$display($time, " << RUN is true >>");  
+		#132 Start= 1'b0;  // bring RUN low again
+		$display($time, " << RUN goes low >>"); 
+		#3950 Start = 1'b0;
+		$display($time, " << Simulation Complete >>");
+      $stop;
+	 end
+	 
+   initial begin
+ // $monitor will print whenever a signal changes
+ // in the design
+   $monitor($time, " clk=%b, Start=%b, SS=%b, SCLK=%b, DF=%b, State=%d, GCK=%b",
+	  Clock, Start, SS, SCLK, DoneFlag, State, Gclock);	 
+   end
+		
+		
+endmodule
+
